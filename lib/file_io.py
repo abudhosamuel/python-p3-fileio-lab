9ea2b56355e2ad7a392dd423bc99d806e05690b8
abudhosamuel <abudhosamuel@gmail.com>
2024-09-05 17:09:04 +0300

    file_io

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,23 @@
+
+
 def write_file(file_name, file_content):
-    pass
+    # Add .txt extension to the file_name
+    full_file_name = f"{file_name}.txt"
+    # Open the file in write mode and write the content
+    with open(full_file_name, 'w') as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    # Add .txt extension to the file_name
+    full_file_name = f"{file_name}.txt"
+    # Open the file in append mode and add the content directly
+    with open(full_file_name, 'a') as file:
+        file.write(append_content)
+
 
 def read_file(file_name):
-    pass
+    # Add .txt extension to the file_name
+    full_file_name = f"{file_name}.txt"
+    # Open the file in read mode and return its content
+    with open(full_file_name, 'r') as file:
+        return file.read()
